eb5ba0ba6fd33a411c1974e63eaa38b67554b4e8
miche <marco.buono.2024@gmail.com>
2026-06-01 03:41:41 +0200

    chore: migrate frontend to Vite and fix build

---
+++ frontend/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@/index.css";
+import App from "@/App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
+
+if ("serviceWorker" in navigator && import.meta.env.PROD) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").catch((err) => {
+      console.error("[ScoreLib] Service worker registration failed", err);
+    });
+  });
+}
